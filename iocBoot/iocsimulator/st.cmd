5d8214756c0653fc4725ece3391bded37007c1bd
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-08-01 11:43:00 -0300

    app: add initial simulator configuration.
    This includes the set of plugins currently compatible with GUIs
    available at LNLS Sirius beamlines. The exception here is ffmpegServer
    and ffmpegFile plugins, which are outside ADCore and have not been
    added to EPICS in Docker yet.
    
    `CONFIG_SITE` from `areaDetector` support module has been included in
    the configuration to make use of the support libraries definition used
    in the commonDriverMakefile definitions.

--- iocBoot/iocsimulator/st.cmd
+++ iocBoot/iocsimulator/st.cmd
@@ -6,4 +6,28 @@
 dbLoadDatabase "../../dbd/simulator.dbd"
 simulator_registerRecordDeviceDriver(pdbbase) 
 
+# IOC and device specific configuration
+epicsEnvSet("PREFIX", "SIM:")
+epicsEnvSet("MAX_IMAGE_WIDTH", 1024)
+epicsEnvSet("MAX_IMAGE_HEIGHT", 1024)
+epicsEnvSet("MAX_IMAGE_PIXELS", 1232896)
+
+# Define simulator driver port
+epicsEnvSet("PORT", "SIM")
+epicsEnvSet("EPICS_CA_AUTO_ARRAY_BYTES", "YES")
+# Include ADCore to path for loading its databases relatively
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+
+simDetectorConfig("$(PORT)", $(MAX_IMAGE_WIDTH), $(MAX_IMAGE_HEIGHT), 1, 0, 0)
+dbLoadRecords("$(ADSIMDETECTOR)/db/simDetector.template", "P=$(PREFIX), R=cam1:, PORT=$(PORT), ADDR=0, TIMEOUT=1")
+
+# Trace error and warning messages
+asynSetTraceMask("$(PORT)", 0, ERROR | WARNING)
+
+# Configure areaDetector plugins
+epicsEnvSet("QSIZE", 20)
+epicsEnvSet("QSIZE_HDF5", 50)
+
+< plugins.cmd
+
 iocInit()
